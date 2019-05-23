e1eb4b0c6ea62234c1fcb1ff7e2c2ca6891d165e
lakbiri1 <44807167+lakbiri1@users.noreply.github.com>
2019-05-23 10:38:27 +0200

    Update addmem.vue

--- smart_doodle_front/doodle_front/src/components/addmem.vue
+++ smart_doodle_front/doodle_front/src/components/addmem.vue
@@ -1,225 +1,55 @@
-<head>
+<template>
+  <div class="event">
+    <header class="header">
+      <h1>Doodle++</h1>
+      <h2>Description de votre événement </h2>
+    </header>
 
-<style type="text/css">
-/* Include the padding and border in an element's total width and height */
-* {
-  box-sizing: border-box;
-}
+    <form @submit="submitForm">
 
-/* Remove margins and padding from the list */
-ul {
-  margin: 0;
-  padding: 0;
-}
+      <p>
+        <label for="description" v-if="description">Description</label>
+        <input id="description" v-model="description" type="text" name="description" placeholder=" saissir la discription" required>
+      </p>
 
-/* Style the list items */
-ul li {
-  cursor: pointer;
-  position: relative;
-  padding: 12px 8px 12px 40px;
-  background: #eee;
-  font-size: 18px;
-  transition: 0.2s;
+      <p>
+        <label for="mail" v-if="mail">Votre mail</label>
+        <input id="mail" v-model="mail" type="email" lieu="mail" placeholder="saissir votre mail" required>
+      </p>
 
-  /* make the list items unselectable */
-  -webkit-user-select: none;
-  -moz-user-select: none;
-  -ms-user-select: none;
-  user-select: none;
-}
+      <p>
+        <input type="submit" value="Continuer" @click="onclick">
+      </p>
+    </form>
 
-/* Set all odd list items to a different color (zebra-stripes) */
-ul li:nth-child(odd) {
-  background: #f9f9f9;
-}
+  </div>
+</template>
 
-/* Darker background-color on hover */
-ul li:hover {
-  background: #ddd;
-}
-
-/* When clicked on, add a background color and strike out text */
-ul li.checked {
-  background: #888;
-  color: #fff;
-  text-decoration: line-through;
-}
-
-/* Add a "checked" mark when clicked on */
-ul li.checked::before {
-  content: '';
-  position: absolute;
-  border-color: #fff;
-  border-style: solid;
-  border-width: 0 2px 2px 0;
-  top: 10px;
-  left: 16px;
-  transform: rotate(45deg);
-  height: 15px;
-  width: 7px;
-}
-
-/* Style the close button */
-.close {
-  position: absolute;
-  right: 0;
-  top: 0;
-  padding: 12px 16px 12px 16px;
-}
-
-.close:hover {
-  background-color: #f44336;
-  color: white;
-}
-
-/* Style the header */
-.header {
-  background-color: #f44336;
-  padding: 30px 40px;
-  color: white;
-  text-align: center;
-}
-
-/* Clear floats after the header */
-.header:after {
-  content: "";
-  display: table;
-  clear: both;
-}
-
-body{
-    background-color: #5dade2;
-    font-family: Helvetica, sans-serif;
-}
-/* Style the input */
-input {
-  margin: 0;
-  border: none;
-  border-radius: 0;
-  width: 75%;
-  padding: 10px;
-  float: left;
-  font-size: 16px;
-}
-
-/* Style the "Add" button */
-.addBtn {
-  padding: 10px;
-  width: 25%;
-  background: #d9d9d9;
-  color: #555;
-  float: left;
-  text-align: center;
-  font-size: 16px;
-  cursor: pointer;
-  transition: 0.3s;
-  border-radius: 0;
-}
-form button[type="submit"] {
-    width: 100%;
-  
-    margin-bottom: 32px;
-    padding: 16px;
-  
-    color: #333;
-
-    border: 1px solid #ccc;
-    border-radius: 5px;
-
-    outline: 0;
-}
-
-form button[type="submit"] {
-    font-weight: bold;
-    color: #eee;
-    text-transform: uppercase;
-
-    background-color: white;
-}
-
-form button[type="submit"]:focus,
-form button[type="submit"]:hover {
-    background-color: #d35400;
-}
-
-.addBtn:hover {
-  background-color: #bbb;
-}
-</style>
-</head>
-<body>
-<div class="wrapper">
-    <p>DoodleSmart</p>
-
-    <section class="login-container">
-         <div id="myDIV" class="header">
-  <h2>Ajouter les membres</h2>
-  <input type="email" id="myInput" v-model="newUser.email" placeholder="email@email.com" required="required"/>
-  <span onclick="newElement()" class="addBtn">Add</span>
-</div>
-
-<ul id="myUL">
-</ul> 
-    </section>
-<form action="" method="post">
-<button type="submit">Continue</button>
-</form>
-</div>
 <script>
-var myNodelist = document.getElementsByTagName("LI");
-var i;
-for (i = 0; i < myNodelist.length; i++) {
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  myNodelist[i].appendChild(span);
-}
-
-// Click on a close button to hide the current list item
-var close = document.getElementsByClassName("close");
-var i;
-for (i = 0; i < close.length; i++) {
-  close[i].onclick = function() {
-    var div = this.parentElement;
-    div.style.display = "none";
-  }
-}
-
-// Add a "checked" symbol when clicking on a list item
-var list = document.querySelector('ul');
-list.addEventListener('click', function(ev) {
-  if (ev.target.tagName === 'LI') {
-    ev.target.classList.toggle('checked');
-  }
-}, false);
-
-// Create a new list item when clicking on the "Add" button
-function newElement() {
-  var li = document.createElement("li");
-  var inputValue = document.getElementById("myInput").value;
-  var t = document.createTextNode(inputValue);
-  li.appendChild(t);
-  if (inputValue === '') {
-    alert("You must write something!");
-  } else {
-    document.getElementById("myUL").appendChild(li);
-  }
-  document.getElementById("myInput").value = "";
-
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  li.appendChild(span);
-
-  for (i = 0; i < close.length; i++) {
-    close[i].onclick = function() {
-      var div = this.parentElement;
-      div.style.display = "none";
+import axios from 'axios'
+export default {
+  data () {
+    return {
+      errors: [],
+      description: null,
+      mail: null,
+    }
+  },
+  props : {
+    onclick: { type : Function }
+  },
+  methods: {
+    submitForm (e) {
+      e.preventDefault()
+      axios.post('http://148.60.11.233/polls', {
+        description: this.description,
+        mail: this.mail
+      }).then(response => {
+        console.log(response)
+      })
     }
   }
 }
 </script>
 
-</body>
+<style src="@/style/event.css"></style>
