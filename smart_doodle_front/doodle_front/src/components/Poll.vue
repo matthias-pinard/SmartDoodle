e84a01d0c56c6a31dc030bcb465c8f09ded4ac49
lakbiri1 <44807167+lakbiri1@users.noreply.github.com>
2019-05-27 17:52:17 +0200

    Update Poll.vue

--- smart_doodle_front/doodle_front/src/components/Poll.vue
+++ smart_doodle_front/doodle_front/src/components/Poll.vue
@@ -1,61 +1,67 @@
 <template>
-  <div class="event">
-    <header class="header">
-      <h1>Doodle++</h1>
-      <h2>Créez votre événement </h2>
-    </header>
+  <id>
+    <h1>Le titre de la réunion : {{ this.event.title }}</h1>
+     <p>by {{this.event.nom}}</p>
+     <p>Lieu : {{this.event.lieu}}</p>
+     <p> la discription de la reunion : {{this.event.summary}}</p>
+      <div class="container" id="tuto">
+      <br>
+ 
+      <div class="panel panel-primary" v-show="this.event.jour.length">
+        <div class="panel-heading">Member</div>        
+        <table class="table table-bordered table-striped">
+          <thead>
+          <th class="col-sm-5">membre</th>
+            <tr v-for="date in dates.length">
+             <th class="col-sm-5">{{date.jour}}+de+{{date.debut}}+à+{{date.fin}}</th>
+            </tr>
+          </thead>
+          <tbody>
+          <tr v-for="date in ">
+          <tr v-for="p in participant">
+          <td>{{ p.nom }}</td>
+            <tr v-for="date in this.event.jour">
+            <td><input type="checkbox" name="nom_variable_1" ></td>
+            </tr> 
+            </tr> 
+          </tbody>       
+        </table>
+        
+      </div> 
 
-    <form @submit="submitForm">
-
-      <p>
-        <label for="title" v-if="title">Titre</label>
-        <input id="title" v-model="title" type="text" name="title" placeholder="Titre" required>
-      </p>
-
-      <p>
-        <label for="lieu" v-if="lieu">Lieu</label>
-        <input id="lieu" v-model="lieu" type="text" lieu="lieu" placeholder="Lieu (facultatif)">
-      </p>
-
-      <p>
-        <label for="summary" v-if="summary">Description</label>
-        <input id="summary" v-model="summary" type="text" summary="summary" placeholder="Description" required>
-      </p>
-
-      <p>
+  </id>
+  <p>
         <input type="submit" value="Continuer" @click="onclick">
       </p>
-    </form>
-
-  </div>
 </template>
 
 <script>
 import axios from 'axios'
 export default {
-  data () {
+  data() {
     return {
-      errors: [],
-      title: null,
-      lieu: null,
-      summary: null
-    }
+      event: null,
+      dates: [
+            {jour: "20/10/2019", debut: "9:00", fin : " 10:00"},
+             {jour: "20/10/2019", debut: "11:00", fin : " 14:00"},
+             {jour: "20/10/2019", debut: "20:00", fin : " 22:00"}          
+          ],
+          participant:[ {nom: "Durand"},            
+            {nom: "Martin"},
+            {nom: "Torlet"} ]
+    };
   },
-  props : {
-    onclick: { type : Function }
-  },
-  methods: {
-    submitForm (e) {
-      e.preventDefault()
-      axios.post('http://148.60.11.233/polls', {
-        title: this.title,
-        summary: this.summary
-      }).then(response => {
-        console.log(response)
-      })
-    }
+  mounted() {
+    axios
+      .get("http://148.60.11.233/polls/" + this.$route.params.id)
+      .then(response => {
+        this.event = response.data;
+      });
   }
-}
+};
+
+
 </script>
 
-<style src="@/style/event.css"></style>
+<style src="@/style/event.css">
+</style>
