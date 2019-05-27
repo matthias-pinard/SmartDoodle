5c893224de763a7c435ca12dc8ef0f20fe8058d7
Squlalah <44104621+Squlalah@users.noreply.github.com>
2019-05-27 16:32:09 +0200

    Front (#6)
    * Création component Event (titre, lieu, description)
    
    * Avancement component Event
    
    * Component pour le choix de dates
    
    * creation d'evenement
    
    * création event + date
    
    * creator + début de la page poll

--- smart_doodle_front/doodle_front/src/App.vue
+++ smart_doodle_front/doodle_front/src/App.vue
@@ -1,33 +1,14 @@
 
 <template>
   <div id="app">
-    <event v-if="state === 0" :onclick="next"></event>
-    <date v-if="state === 1" :id="id"></date>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import Event from "./components/Event";
-import Date from "./components/Date";
 export default {
-  data: function() {
-    return {
-      state: 0,
-      id: null
-    };
-  },
-
-  methods: {
-    next: function () {
-      this.state = 1
-    }
-  },
-
-  components: {
-    Event,
-    Date
-  }
-};
+  name: 'App'
+}
 </script>
 
 
---
+++ smart_doodle_front/doodle_front/src/components/Poll.vue
@@ -0,0 +1,28 @@
+<template>
+  <id>
+    <h1>{{ this.event.title }}</h1>
+  </id>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  data() {
+    return {
+      event: null
+    };
+  },
+
+  mounted() {
+    axios
+      .get("http://148.60.11.233/polls/" + this.$route.params.id)
+      .then(response => {
+        this.event = response.data;
+      });
+  }
+};
+</script>
+
+<style src="@/style/event.css">
+</style>
+
