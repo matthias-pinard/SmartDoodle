9d33a5b08319e5e3a2ad85bee1a21e54759a2be5
lakbiri1 <44807167+lakbiri1@users.noreply.github.com>
2019-05-28 14:34:04 +0200

    Update and rename Poll.vue to tableau.vue

---
+++ smart_doodle_front/doodle_front/src/components/tableau.vue
@@ -0,0 +1,51 @@
+<template>
+        <table>
+      <thead>
+        <tr>
+          <td>&nbsp;</td>
+          <template v-for="date in dates">
+           <td>{{date.jour}}+de+{{date.debut}}+à+{{date.fin}}</td>
+        </template>
+          </tr>
+      </thead>
+      <tbody repeat.for ="p of participant"></tbody>
+        <tr>
+        <td>{{ p.nom }}</td>
+        <template v-for="date in dates">
+          <td ><input type="checkbox" id="checkbox" v-model="checked" ></td>
+        </template>     
+        </tr>
+      </tbody>
+    </table>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  data() {
+    return {
+      event: null,
+      dates: [
+            {jour: "20/10/2019", debut: "9:00", fin : " 10:00"},
+             {jour: "20/10/2019", debut: "11:00", fin : " 14:00"},
+             {jour: "20/10/2019", debut: "20:00", fin : " 22:00"}          
+          ],
+          participant:[ {nom: "Durand"},            
+            {nom: "Martin"},
+            {nom: "Torlet"} ]
+    };
+  },
+  mounted() {
+    axios
+      .get("http://148.60.11.233/polls/" + this.$route.params.id)
+      .then(response => {
+        this.event = response.data;
+      });
+  }
+};
+
+
+</script>
+
+<style src="@/style/event.css">
+</style>
