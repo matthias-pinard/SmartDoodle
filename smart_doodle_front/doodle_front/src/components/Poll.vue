420882702089f11e9e6ecde1eeb4dcd3085cfa0f
Nassafy <matthiaspinard@aol.com>
2019-05-29 08:43:29 +0200

    Front (#8)
    * Création component Event (titre, lieu, description)
    
    * Avancement component Event
    
    * Component pour le choix de dates
    
    * creation d'evenement
    
    * création event + date
    
    * creator + début de la page poll
    
    * poursuite page sondage + changements mineurs création
    
    * deploiement github
    
    * deploy
    
    * deploy

---
+++ smart_doodle_front/doodle_front/src/components/Poll.vue
@@ -0,0 +1,39 @@
+<template>
+  <id>
+    <h1>{{ this.event.title }}</h1>
+    <h2>Evénement créé par {{ this.event.guests[0].name }}</h2>
+    <h3>{{ this.event.summary }}</h3>
+    <p>
+      Partagez le lien de la page pour inviter de nouveaux participants
+      </br>
+      </br>
+      <a :href="this.event.pad_link" target="_blank">Accéder au pad</a>
+    </p>
+  </id>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  data() {
+    return {
+      event: null,
+      url: null
+    };
+  },
+
+  mounted() {
+    axios
+      .get("http://148.60.11.233/polls/" + this.$route.params.id)
+      .then(response => {
+        this.event = response.data;
+      });
+    this.url = location.href;
+    console.log(location.href);
+  }
+};
+</script>
+
+<style src="@/style/event.css">
+</style>
+
